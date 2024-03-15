8dc3b894ae5a0b3512e78672d9e9406282516daa
drozd86sashunya <152299288+drozd86sashunya@users.noreply.github.com>
2024-03-15 16:48:39 +0000

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,27 @@
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    main()
+    return data
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+    data = generate_random_data()
+def main():
+def generate_random_data():
+
+    for item in data:    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def generate_random_data():
+import random
+
+    main()
+def main():
+
+        print(f"Random Number: {item}")
+
+
+    for item in data:
+    return data
+if __name__ == "__main__":
